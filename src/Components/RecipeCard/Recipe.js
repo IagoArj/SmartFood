13b85782cb2a55e98145c90a57059b69439f029d
IagoArj <emersoniago1995@gmail.com>
2023-04-25 21:41:28 -0300

    script para normalizar titulo e sumario, correção do bug do card, focar em resolver problemas de responsividade

--- src/Components/RecipeCard/Recipe.js
+++ src/Components/RecipeCard/Recipe.js
@@ -3,19 +3,41 @@ import FastInformation from '../FastInformation/FastInformation';
 import './Recipe.css'
 function RecipeCard(props) {
 
+    function changeTextSize(text, size) {
+        if (text.length >= size) {
+            return <p className='recipe-card-title-big'>{text}</p>
+        }
+        else {
+            return <p className='recipe-card-title-normal'>{text}</p>
+        }
+    }
+    function normalizeText(text, maxChar) {
+        let normalize = text.replace(/<[^>]*>/g, '');
+        if (normalize.length > maxChar) {
+            normalize = normalize.slice(0, maxChar) + '...';
+        }
+
+
+        return normalize
+
+    }
 
     return (
         <div className='recipe-box'>
             <img className='recipe-image' src={props.data.image}></img>
             <div className='card-information-box'>
                 <div className='card-information'>
-                    <p className='recipe-card-title'>{props.data.title}</p>
+                    <div>
+                        {changeTextSize(props.data.title, 30)}
+                    </div>
                     <div className='fast-information-box'>
                         <FastInformation data={props.data.servings} infoType={true} />
                         <FastInformation data={props.data.readyInMinutes} infoType={false} />
                     </div>
+                    <div className='summary-box'>
+                        <p className='summary-text'>{normalizeText(props.data.summary, 100)}</p>
+                    </div>
 
-                    <h2>testes e mais testes</h2>
                     <ButtonRecipeCard title="Read More" />
                 </div>
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,46 +3,50 @@ import './Home.css'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import RecipeCard from '../Components/RecipeCard/Recipe';
-function Home () {
+function Home() {
 
-    
-        const [recipes, setRecipes] = useState();
-        const baseurl = 'https://api.spoonacular.com';
-        const apiKey = '4d29cc01829749d08783cec3575e0c28'
-        const authAxios = axios.create({
-            baseURL: baseurl,
-            headers:{
-                'x-api-key':  apiKey
-            }
-        });
-       
 
-        const getRecipes = async () =>{
-            try {
-                const response = await authAxios.get('/recipes/random?number=2')
-                console.log(response.data.recipes)
-                setRecipes(response.data.recipes)
-                
-                
-            } catch (error) {
-                console.log(error)
-            }
+    const [recipes, setRecipes] = useState();
+    const baseurl = 'https://api.spoonacular.com';
+    const apiKey = '4d29cc01829749d08783cec3575e0c28';
+    const cardsQuant = '10';
+    const authAxios = axios.create({
+        baseURL: baseurl,
+        headers: {
+            'x-api-key': apiKey
         }
-        useEffect(()=>{
-            getRecipes()
-        },[])
-        return (
-            <div className='teste' >
-                <h1>Home</h1>
-                {recipes?.map((recipe)=>{
-                    
-                    return <RecipeCard data= {recipe} key = {recipe.id} />
+    });
+
+
+    const getRecipes = async () => {
+        try {
+            const response = await authAxios.get('/recipes/random?number=' + cardsQuant)
+            console.log(response.data.recipes)
+
+            setRecipes(response.data.recipes)
+
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        getRecipes()
+    }, [])
+    return (
+        <div className='teste' >
+            <h1>Home</h1>
+            <div className='teste2-home'>
+                {recipes?.map((recipe) => {
+
+                    return <RecipeCard data={recipe} key={recipe.id} />
 
                 })}
             </div>
+        </div>
 
-        );
-    }
+    );
+}
 
 
 export default Home;
